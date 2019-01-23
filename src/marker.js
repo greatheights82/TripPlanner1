80676b4233c86561569795715a7a65ebc2248ab3
Jake Hebert <greatheights@gmail.com>
2019-01-23 16:07:32 -0600

    finished Marker Factory Module

--- src/marker.js
+++ src/marker.js
@@ -1,30 +1,26 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
 const iconURLs = {
-  hotels: "http://i.imgur.com/D9574Cu.png",
-  restaurants: "http://i.imgur.com/cqR6pUI.png",
-  activities: "http://i.imgur.com/WbMOfMl.png"
+  hotels: 'http://i.imgur.com/D9574Cu.png',
+  restaurants: 'http://i.imgur.com/cqR6pUI.png',
+  activities: 'http://i.imgur.com/WbMOfMl.png',
 };
 
-const buildMarker = (type, coords) => {
-  let iconURL = "";
-  if (type === "hotel") {
+const buildMarker = function(type, coords) {
+  let iconURL = '';
+  if (type === 'hotel') {
     iconURL = iconURLs.hotels;
   }
 
-  if (type === "restaurant") {
+  if (type === 'restaurant') {
     iconURL = iconURLs.restaurants;
   }
 
-  if (type === "activity") {
+  if (type === 'activity') {
     iconURL = iconURLs.activities;
   }
 
-  const markerDomEl = document.createElement("div");
-  markerDomEl.style.width = "32px";
-  markerDomEl.style.height = "39px";
-  markerDomEl.style.backgroundImage = `url(${iconURL})`;
-  new mapboxgl.Marker(markerDomEl).setLngLat(coords);
+  return { iconURL, coords };
 };
 
 module.exports = buildMarker;
