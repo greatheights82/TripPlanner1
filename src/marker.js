2501ea667c059f3b3d6fda4fd8a853cfaf941c54
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-23 15:56:21 -0600

    marker is still broken

--- src/marker.js
+++ src/marker.js
@@ -1,32 +1,30 @@
-const mapboxgl = require('mapbox-gl');
+const mapboxgl = require("mapbox-gl");
 
 const iconURLs = {
-  hotels: 'http://i.imgur.com/D9574Cu.png',
-  restaurants: 'http://i.imgur.com/cqR6pUI.png',
-  activities: 'http://i.imgur.com/WbMOfMl.png',
+  hotels: "http://i.imgur.com/D9574Cu.png",
+  restaurants: "http://i.imgur.com/cqR6pUI.png",
+  activities: "http://i.imgur.com/WbMOfMl.png"
 };
 
 const buildMarker = (type, coords) => {
-  let iconURL = '';
-  if (type === 'hotel') {
+  let iconURL = "";
+  if (type === "hotel") {
     iconURL = iconURLs.hotels;
   }
 
-  if (type === 'restaurant') {
+  if (type === "restaurant") {
     iconURL = iconURLs.restaurants;
   }
 
-  if (type === 'activity') {
+  if (type === "activity") {
     iconURL = iconURLs.activities;
   }
 
-  const markerDomEl = document.createElement('div');
-  markerDomEl.style.width = '32px';
-  markerDomEl.style.height = '39px';
+  const markerDomEl = document.createElement("div");
+  markerDomEl.style.width = "32px";
+  markerDomEl.style.height = "39px";
   markerDomEl.style.backgroundImage = `url(${iconURL})`;
-  new mapboxgl.Marker(markerDomEl).setLngLat(coords).addTo(map);
+  new mapboxgl.Marker(markerDomEl).setLngLat(coords);
 };
-
-console.log(buildMarker('hotel', [-74.009, 40.705]));
 
 module.exports = buildMarker;
